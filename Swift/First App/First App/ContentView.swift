9500beb0b396ec74fae198412a64928add4b2cf5
Carl Elgström <elgstrom@macbook-pro.lan>
2020-02-05 10:54:26 -0500

    Swift folder

---
+++ Swift/First App/First App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First App
+//
+//  Created by Carl Elgström  on 2020-02-05.
+//  Copyright © 2020 Carl Elgström . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
